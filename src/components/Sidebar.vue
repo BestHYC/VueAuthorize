15536f6ce82e198c1134d2694832a2a3aae8ebd9
hongyichao <970973742@qq.com>
2018-05-29 18:13:34 +0800

    修改菜单权限限制
    Signed-off-by: hongyichao <970973742@qq.com>

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,16 +7,18 @@
 				</div>
 				<ul class="page-sidebar-menu"  v-for="(item,index) in items" 
 					:key="item.name" v-show="currentIndex == index">
-					<li class="nav-item" v-for="(item1, index1) in item['children']" :key="item1.name">
-						<a href="#" class="nav-link nav-toggle" @click="toggleUl(index1)">
+					<li class="nav-item" v-for="(item1, index1) in item['children']" :key="item1.name" >
+						<a href="#" class="nav-link nav-toggle" @click="clickLi(item1,index,index1)"
+							v-bind:class="{active:current[currentIndex][0]===index1}">
 							<i :class="[item1['iconName']]"></i>
 							<span>{{item1.descriptor}}</span>
 							<span v-if="item1['children']" class="el-icon-more-outline iconleft"></span>
 						</a>
 						<ul class="sub-menu" v-if="item1['children']" 
 							:class="{'showToggle':index1!=current}">
-							<li class="nav-item active" v-for="i in item1['children']" :key="i.name">
-								<a @click.prevent="toRoute(i)">
+							<li class="nav-item" v-for="(i,index2) in item1['children']" :key="i.name" >
+								<a @click.prevent="clickLi(i)" 
+									:class="{active:current[currentIndex][1]===index2}">
 									<i :class="[i['iconName']]"></i>
 									<span>{{i.descriptor}}</span>
 								</a>
@@ -41,7 +43,10 @@
 				required: true
 			}
 		},
-		mounted(){
+		created(){
+			for(let i =0, len = this.items.length; i < len; i++){
+				this.current.push([]);
+			}
 		},
 		computed:{
 		},
@@ -52,7 +57,12 @@
 			toggleUl:function(index){
 				this.current == index?(this.current=NaN):(this.current=index);
 			},
-			clickLi:function(item, index){
+			clickLi:function(item, index,index1, index2){
+				this.current[index][0]=index1				
+				if(!item["children"]){
+					this.current[index][1]=index2;
+					this.toRoute(item)
+				}
 				
 			}
 		},
