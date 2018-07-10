facc1cc01a9b1753ef0045cfeabd156da65f24f4
hongyichao <970973742@qq.com>
2018-07-10 17:18:06 +0800

    操作Enum数据
    Signed-off-by: hongyichao <970973742@qq.com>

---
+++ src/authorize/ModuleEnum.js
@@ -0,0 +1,5 @@
+const ModuleName=Symbol.for("权限属性固定值");
+const MenuName = Symbol.for('按钮名称固定值')
+const jsonName = ['components','meta','redirect'];//针对对象进行序列化,然后存放至数据库中
+export {MenuName,jsonName}
+export default ModuleName;
